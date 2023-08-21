d20dd09ce6d85e018967b11d294bef336eef6ea5
Temir Umurzakov <temir@umurzakov.com>
2023-08-21 17:17:24 +0600

    FIX: funcs copied from wrong place

--- animatediff/models/motion_module.py
+++ animatediff/models/motion_module.py
@@ -255,6 +255,9 @@ class VersatileAttention(CrossAttention):
             *args, **kwargs
         ):
         super().__init__(*args, **kwargs)
+
+        self.num_heads = channels // num_head_channels if num_head_channels is not None else 1
+
         assert attention_mode == "Temporal"
 
         self.attention_mode = attention_mode
@@ -271,18 +274,17 @@ class VersatileAttention(CrossAttention):
 
     def reshape_heads_to_batch_dim(self, tensor):
         batch_size, seq_len, dim = tensor.shape
-        head_size = self.num_heads
+        head_size = self.heads
         tensor = tensor.reshape(batch_size, seq_len, head_size, dim // head_size)
         tensor = tensor.permute(0, 2, 1, 3).reshape(batch_size * head_size, seq_len, dim // head_size)
         return tensor
 
     def reshape_batch_dim_to_heads(self, tensor):
         batch_size, seq_len, dim = tensor.shape
-        head_size = self.num_heads
+        head_size = self.heads
         tensor = tensor.reshape(batch_size // head_size, head_size, seq_len, dim)
         tensor = tensor.permute(0, 2, 1, 3).reshape(batch_size // head_size, seq_len, dim * head_size)
         return tensor
-
 
     def forward(self, hidden_states, encoder_hidden_states=None, attention_mask=None, video_length=None):
         batch_size, sequence_length, _ = hidden_states.shape
