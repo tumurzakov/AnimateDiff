f8976607849f853dd4314fd25897cb77a29dfdaf
Temir Umurzakov <temir@umurzakov.com>
2023-08-28 11:41:16 +0600

    Logging message removed

--- animatediff/models/unet.py
+++ animatediff/models/unet.py
@@ -417,7 +417,7 @@ class UNet3DConditionModel(ModelMixin, ConfigMixin, UNet2DConditionLoadersMixin)
         upsample_size = None
 
         if any(s % default_overall_up_factor != 0 for s in sample.shape[-2:]):
-            logger.info("Forward upsample size to force interpolation output size.")
+            #logger.info("Forward upsample size to force interpolation output size.")
             forward_upsample_size = True
 
         # prepare attention_mask
