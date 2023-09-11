34102d517e7a4f5b5ba4a5d0bd93cee5cff8065c
Temir Umurzakov <temir@umurzakov.com>
2023-09-11 14:51:48 +0600

    Debug facenet

--- animatediff/utils/facenet.py
+++ animatediff/utils/facenet.py
@@ -11,11 +11,14 @@ class Facenet:
         self.reference = self.calc_embedding(reference)
 
     def calc_embedding(self, img):
+        print('facenet', 'input', img.shape)
         img_cropped = self.mtcnn(img)[0]
+        print('facenet', 'mtcnn', img_cropped.shape if img_cropped != None else 'None')
         if img_cropped == None:
             return None
 
         img_embedding = self.resnet(img_cropped.unsqueeze(0).to(self.device))
+        print('facenet', 'embedding', img_embedding.shape)
         return img_embedding
 
     def calc_distance(self, e1, e2):
